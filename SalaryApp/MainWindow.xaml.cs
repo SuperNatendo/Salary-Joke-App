a7b11711b7a2c0c98e1b2acc8169eb9c16b2f398
BOISELLN <BOISELLN@USNAPLT0115>
2021-12-08 14:58:37 -0600

    Add project files.

---
+++ SalaryApp/MainWindow.xaml.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace SalaryApp
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            MessageBox.Show("Great! Have a nice day!");
+            Environment.Exit(0);
+        }
+
+        private void UIElement_OnMouseEnter(object sender, MouseEventArgs e)
+        {
+            var rnd = new Random();
+            AppWindow.Top = rnd.Next(750);
+            AppWindow.Left = rnd.Next(750);
+        }
+
+        private void Button_Click_1(object sender, RoutedEventArgs e)
+        {
+            MessageBox.Show("Hey! You weren't supposed to click this!");
+        }
+    }
+}
